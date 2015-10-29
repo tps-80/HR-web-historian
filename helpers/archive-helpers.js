8e066fb1861cc1d35bcd6777627947e459ade4f0
tps-80 <thomaspsorensen@gmail.com>
2015-10-28 20:46:30 -0700

    Wrote helper functions for archiving urls, checking if urls exist in the .txt file and for returning the content of a an already archived url

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -10,9 +10,11 @@ var _ = require('underscore');
  */
 
 exports.paths = {
-  siteAssets: path.join(__dirname, '../web/public'),
+  siteAssets: path.join(__dirname, '../web/public/'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  home: path.join(__dirname, '../web/public/index.html'),
+  loading: path.join(__dirname, '../web/public/loading.html'),
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -26,15 +28,54 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function() {
+
 };
 
-exports.isUrlInList = function() {
+exports.isUrlInList = function(url) {
+  //check if URL already exists inside sites.txt
+    //if it exists, go into sites folder and serve up the site
+    //if it doesn't exist, add it to sites.txt
+
 };
 
 exports.addUrlToList = function() {
+
 };
 
-exports.isUrlArchived = function() {
+exports.isUrlArchived = function(url, res) {
+// check if the file specified by url exists in the testdata/sites folder
+// if it does, return the html file within the archive
+  var fullPath = exports.paths.archivedSites + url
+  fs.exists(fullPath, function(exists){
+    if (exists) {
+      fs.readFile(fullPath, function(err, html){
+        if (err) {
+          console.log(err);
+        }
+        var content = html.toString();
+        console.log(content);
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(content);  
+        res.end();
+      })
+    } else {
+      res.writeHeader(404);
+      res.end()
+    }
+  })
+
+  // fs.readFile(exports.paths.list, function(err, data) {
+  //     if(err) {
+  //       throw err;
+  //     };
+  //     var array = data.toString().split("\n");
+  //     for(i in array) {
+
+  //       if (array[i] === url) {
+
+  //       }
+  //     }
+  // });
 };
 
 exports.downloadUrls = function() {
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,42 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  // if get request, run isURLarchived(req.url), false
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      fs.readFile(archive.paths.home, function(err, html){
+        if (err) {
+          console.log(err);
+        }
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(html);  
+        res.end();
+      })
+    } else if (req.url === "/styles.css" || req.url === "/favicon.ico") {
+      res.end()
+    } else {
+      archive.isUrlArchived(req.url, res);
+    }
+  } else if (req.method === "POST") {
+    var data = '';
+    req.on('data', function(datum){
+      data+=datum;
+    })
+    req.on('end', function(err){
+      var site = data.slice(4) + "\n";
+      fs.readFile(archive.paths.loading, function(err, html){
+        if (err) {
+          console.log(err);
+        }
+        res.writeHead(302);  
+        res.write(html);  
+        res.end();
+      })
+      fs.appendFileSync(archive.paths.list, site);
+    });
+  }
 };
